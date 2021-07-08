a84d748dcc18dfc30fabca981cec84340376a35f
Hernan Gomez Molino <hernangomezmolino@gmail.com>
2021-07-07 22:15:13 -0300

    se agregan teclas.c y teclas. h

---
+++ inc/teclas.h
@@ -0,0 +1,44 @@
+/*=============================================================================
+ * Authors: Carlos Maffrand 	<carlosmaffrand5@gmail.com>
+ *			Hernan Gomez Molino	<hernangomezmolino@gmail.com>
+ * Date: 2021/07/07
+ *===========================================================================*/
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef __TECLAS_H__
+#define __TECLAS_H__
+
+/*=====[Inclusions of public function dependencies]==========================*/
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*=====[C++ - begin]=========================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*=====[Definition macros of public constants]===============================*/
+
+/*=====[Public function-like macros]=========================================*/
+
+/*=====[Definitions of public data types]====================================*/
+
+/*=====[Prototypes (declarations) of public functions]=======================*/
+
+/*=====[Prototypes (declarations) of public interrupt functions]=============*/
+
+
+bool_t leerTecla (gpioMap_t tecla);
+
+/*=====[C++ - end]===========================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*=====[Avoid multiple inclusion - end]======================================*/
+
+#endif /* __TECLAS_H__ */
---
+++ src/teclas.c
@@ -0,0 +1,23 @@
+/*=============================================================================
+ * Authors: Carlos Maffrand 	<carlosmaffrand5@gmail.com>
+ *			Hernan Gomez Molino	<hernangomezmolino@gmail.com>
+ * Date: 2021/07/07
+ *===========================================================================*/
+#include "teclas.h"
+//#include "sapi.h"
+
+bool_t leerTecla (gpioMap_t tecla)
+{
+	bool_t ret_val;
+
+   if ((tecla == TEC1) || (tecla == TEC2) || (tecla == TEC3) || (tecla == TEC4)) {
+	   ret_val = gpioRead( tecla );
+   }
+   else {
+   // No se puede leer ninguna tecla.
+   ret_val     = 1;
+   printf("\n Error: Se intentó presionar una tecla no permitida");
+   }
+
+	return ret_val;
+}
